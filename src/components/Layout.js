221953b63324089cfa8936247ecd91caeef60708
Kayot88 <kayot88@gmail.com>
2020-01-07 19:41:16 +0200

    fk

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 
 // import "./bootstrap.min.css"
-import "./layout"
+import "./layout.css"
 // console.log(`${__dirname}src/pages`);
 const Layout = ({ children }) => <>{children}</>
 
